767630e848b245f85d79c92f8743c639ac6b81fc
Sauvik Ray <sauvikray001@gmail.com>
2023-05-10 12:04:12 +0600

    pre merge branch

--- lib/utils/helpers/space_helper.dart
+++ lib/utils/helpers/space_helper.dart
@@ -8,14 +8,14 @@ class UIHelper {
   // Vertical spacing constants. Adjust to your liking.
   static final double _verticalSpaceSmall = 10.0.w;
   static final double _verticalSpaceMedium = 20.0.w;
-  static final double _verticalSpaceSemiLarge = 40.0.w;
+  static final double _verticalSpaceSemiLarge = 30.0.w;
   static final double _verticalSpaceLarge = 60.0.w;
   static final double _verticalSpaceExtraLarge = 100.0.w;
 
   // Vertical spacing constants. Adjust to your liking.
   static final double _horizontalSpaceSmall = 10.0.h;
   static final double _horizontalSpaceMedium = 20.0.h;
-  static final double _horizontalSpaceSemiLarge = 40.0.h;
+  static final double _horizontalSpaceSemiLarge = 30.0.h;
   static final double _horizontalSpaceLarge = 60.0.h;
 
   static Widget verticalSpaceSmall = SizedBox(height: _verticalSpaceSmall);
